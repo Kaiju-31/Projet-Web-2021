f1906257b46a18d0347847b08edd8096425a347f
G-I-G-A <63502991+G-I-G-A@users.noreply.github.com>
2021-01-11 20:42:44 +0100

    [update] show

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -8,7 +8,7 @@
     <div class="mw-full"> <!-- w-600 = width: 60rem (600px), mw-full = max-width: 100% -->
         <div class="card p-1"> <!-- p-0 = padding: 0 -->
             <div class="row">
-            <img src="{{ $game->image }}" class="img-fluid rounded-top" alt="..."> <!-- rounded-top = rounded corners on the top -->
+            <img src="{{ $game->image }}" class="img-fluid rounded-top"> <!-- rounded-top = rounded corners on the top -->
             <!-- First content container nested inside card -->
             <div class="content">
                 <h1 class="content-title">
@@ -32,7 +32,11 @@
                                 <i class="fas fa-gamepad text-danger"></i>
                         @endif
                     </span>
+                    <span class="text-muted">
+                         {{$game->plateform}}
+                    </span>
                 </div>
+                <br>
                 <div>
                     <span class="badge">
                         <i class="fa fa-comments text-justify mr-3" aria-hidden="true"></i> 5 comments <!-- text-primary = color: primary-color, mr-5 = margin-right: 0.5rem (5px) -->
@@ -43,6 +47,19 @@
                     <span class="badge ml-5"> <!-- ml-5 = margin-left: 0.5rem (5px) -->
                         <i class="fas fa-tag text-black-50 mr-3"> {{$game->price}}</i><!-- text-danger = color: danger-color, mr-5 = margin-right: 0.5rem (5px) -->
                     </span>
+                </div>
+                <br>
+                <div>
+                    <label>Quantity :</label>
+                    @if($game->quantity > 20)
+                    <div class="progress h-25"> <!-- h-25 = height: 2.5rem (25px) -->
+                        <div class="progress-bar rounded-0 bg-success" role="progressbar" style="width: {{$game->quantity}}%" aria-valuenow="{{$game->quantity}}" aria-valuemin="0" aria-valuemax="100">{{$game->quantity}}</div> <!-- w-three-quarter = width: 75%, rounded-0 = border-radius: 0 -->
+                    </div>
+                    @else
+                    <div class="progress h-25"> <!-- h-25 = height: 2.5rem (25px) -->
+                        <div class="progress-bar rounded-0 bg-danger" role="progressbar" style="width: {{$game->quantity}}%" aria-valuenow="{{$game->quantity}}" aria-valuemin="0" aria-valuemax="100">{{$game->quantity}}</div> <!-- w-three-quarter = width: 75%, rounded-0 = border-radius: 0 -->
+                    </div>
+                    @endif
                 </div>
             </div>
             </div>
