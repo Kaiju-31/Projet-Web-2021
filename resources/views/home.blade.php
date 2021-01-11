0ab5b1bc265569c0284cfb378299c7fefbbbdfc0
Kaiju-31 <rodolf.chotard@ynov.com>
2021-01-11 18:41:26 +0100

    [Update] Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,13 +9,18 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header"><h1>Dashboard</h1></div>
+                    <div class="card-header"><h1> Dashboard </h1></div>
 
                     <div class="card-body">
                         @if(auth()->user()->is_admin == 1)
-                            <a href="{{url('admin/routes')}}">Admin {{ auth()->user()->name }}</a>
+                            <a href="{{url('admin/routes')}}"> Admin {{ auth()->user()->name }} </a>
                         @else
-                            <div class=”panel-heading”><h2>{{ auth()->user()->name }}</h2></div>
+                            <div class=”panel-heading”><h2> {{ auth()->user()->name }} </h2></div>
+                            <span>Mail : {{ auth()->user()->email }} </span>
+                            <p>Solde : {{ auth()->user()->balance }} €</p>
+                            @foreach()
+
+                            @endforeach
                         @endif
                     </div>
                 </div>
