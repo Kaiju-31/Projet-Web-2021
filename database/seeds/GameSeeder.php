8be351cdfcc1f2d50be37ebb5f36ffc2200961c6
Kaiju-31 <71188826+Kaiju-31@users.noreply.github.com>
2021-01-28 01:09:48 +0100

    [DELETE] GameSeeder/Table

--- database/seeds/GameSeeder.php
+++ database/seeds/GameSeeder.php
@@ -20,7 +20,6 @@ class GameSeeder extends Seeder
                     "quantity" => 13,
                     "price" => 69.99,
                     "plateform" => "PS5",
-                    "activation_code" => "zKcxNSRMLGC4LeNuQ3Hm"
                 ],
                 [
                     "image" => "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Feditioncollector.fr%2Fwp-content%2Fuploads%2F2020%2F07%2FThe-Last-of-Us-Part-II-cd-musiques-bande-originale.jpg&f=1&nofb=1",
@@ -29,7 +28,6 @@ class GameSeeder extends Seeder
                     "quantity" => 13,
                     "price" => 69.99,
                     "plateform" => "PS5",
-                    "activation_code" => "8LAxboszeXq9jHj8yry9"
                 ],
                 [
                     "image" => "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Feditioncollector.fr%2Fwp-content%2Fuploads%2F2020%2F07%2FThe-Last-of-Us-Part-II-cd-musiques-bande-originale.jpg&f=1&nofb=1",
@@ -38,7 +36,6 @@ class GameSeeder extends Seeder
                     "quantity" => 13,
                     "price" => 69.99,
                     "plateform" => "PS5",
-                    "activation_code" => "jFnwjz5AsQr2PEowroSB"
                 ],
                 [
                     "image" => "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Feditioncollector.fr%2Fwp-content%2Fuploads%2F2020%2F07%2FThe-Last-of-Us-Part-II-cd-musiques-bande-originale.jpg&f=1&nofb=1",
@@ -47,7 +44,6 @@ class GameSeeder extends Seeder
                     "quantity" => 13,
                     "price" => 69.99,
                     "plateform" => "PS5",
-                    "activation_code" => "uRy9jXdHQrHgwbBH2Pk5"
                 ],
                 [
                     "image" => "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Feditioncollector.fr%2Fwp-content%2Fuploads%2F2020%2F07%2FThe-Last-of-Us-Part-II-cd-musiques-bande-originale.jpg&f=1&nofb=1",
@@ -56,7 +52,6 @@ class GameSeeder extends Seeder
                     "quantity" => 13,
                     "price" => 69.99,
                     "plateform" => "PS5",
-                    "activation_code" => "5tfmEm7h72b9KCY3zQNR"
                 ],
                 [
                     "image" => "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Feditioncollector.fr%2Fwp-content%2Fuploads%2F2020%2F07%2FThe-Last-of-Us-Part-II-cd-musiques-bande-originale.jpg&f=1&nofb=1",
@@ -65,7 +60,6 @@ class GameSeeder extends Seeder
                     "quantity" => 13,
                     "price" => 69.99,
                     "plateform" => "PS5",
-                    "activation_code" => "PCnk8rttdmCGgYKbXa8H"
                 ],
             ]
         );
