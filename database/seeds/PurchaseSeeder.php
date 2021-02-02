31af59c7f0b131e1a5e3c08baed88aa76a4cc12e
Kaiju-31 <71188826+Kaiju-31@users.noreply.github.com>
2021-02-02 23:50:15 +0100

    final

--- database/seeds/PurchaseSeeder.php
+++ database/seeds/PurchaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PurchaseSeeder extends Seeder
 {
@@ -11,6 +12,24 @@ class PurchaseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('purchases')->insert(
+            [
+                [
+                    "date_purchase" => "2021-02-02",
+                    "game_id" => "1",
+                    "user_id" => "1",
+                ],
+                [
+                    "date_purchase" => "2021-02-02",
+                    "game_id" => "2",
+                    "user_id" => "1",
+                ],
+                [
+                    "date_purchase" => "2021-02-02",
+                    "game_id" => "3",
+                    "user_id" => "1",
+                ],
+            ]
+        );
     }
 }
